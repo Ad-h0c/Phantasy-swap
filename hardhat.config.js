8c5059655fad8d058c3d3e08e4de3422ec9bacc2
Ad-h0c <reddycsivaprakash5@gmail.com>
2022-10-20 14:23:21 +0530

    Configuration, solidity, and js files are added

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,35 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-chai-matchers");
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+require("@typechain/hardhat");
+require("hardhat-gas-reporter");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const {
+  GOERLI_PRIVATE_KEY,
+  REACT_APP_ALCHEMY_API_KEY,
+  YOUR_ETHERSCAN_API_KEY,
+  QUICKNODE_API_KEY,
+} = process.env;
+
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${REACT_APP_ALCHEMY_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY],
+    },
+    bsctest: {
+      url: `https://radial-wispy-dinghy.bsc-testnet.discover.quiknode.pro/${QUICKNODE_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: YOUR_ETHERSCAN_API_KEY,
+  },
 };
