49250a69bf170fcb88ea490f58b664e22d74bf2c
Ad-h0c <reddycsivaprakash5@gmail.com>
2022-11-06 20:29:11 +0530

    Added erc20 token contract

---
+++ contracts/PhantasyERC20.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Identifier: DEFI
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Elixir is ERC20("Elixir","elxr"),Ownable {
+
+  function mint(uint256 amount ) public onlyOwner returns (bool) {
+    _mint(_msgSender(), amount);
+    return true;
+  }
+
+  function burn(uint256 amount) public onlyOwner returns (bool) {
+    _burn(_msgSender(), amount);
+    return true;
+  }
+}
+
+
+
+
+
